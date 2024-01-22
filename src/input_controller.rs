8584ab5ff81baa877bb7afef00fdebc685805a89
Artem Fokin <artem.v.fokin@gmail.com>
2024-01-22 11:39:27 +0400

    Extract InputController to separate file

---
+++ src/input_controller.rs
@@ -0,0 +1,88 @@
+use crate::EntityEventType::StartMove;
+use crate::{screen_to_world, EntityEvent, Position, Properties};
+use hecs::World;
+use sdl2::event::Event;
+use sdl2::keyboard::Keycode;
+use sdl2::mouse::MouseButton;
+use sdl2::{EventPump, Sdl};
+
+pub struct InputController {
+    sdl_events: EventPump,
+}
+
+impl InputController {
+    pub fn new(sdl_context: &Sdl) -> Self {
+        Self {
+            sdl_events: sdl_context.event_pump().unwrap(),
+        }
+    }
+
+    pub fn update(
+        &mut self,
+        properties: &mut Properties,
+        entity_events: &mut Vec<EntityEvent>,
+        world: &mut World,
+    ) {
+        for event in self.sdl_events.poll_iter() {
+            match event {
+                Event::Quit { .. }
+                | Event::KeyDown {
+                    keycode: Some(Keycode::Escape),
+                    ..
+                } => properties.quit = true,
+                Event::MouseButtonUp {
+                    mouse_btn: MouseButton::Left,
+                    x,
+                    y,
+                    ..
+                } => left_mouse_click(x, y, properties, world),
+                Event::MouseButtonUp {
+                    mouse_btn: MouseButton::Right,
+                    x,
+                    y,
+                    ..
+                } => right_mouse_click(x, y, properties, entity_events),
+                _ => {}
+            }
+        }
+    }
+}
+
+fn left_mouse_click(x_screen: i32, y_screen: i32, properties: &mut Properties, world: &mut World) {
+    let x_world = screen_to_world(x_screen, 50);
+    let y_world = screen_to_world(y_screen, 50);
+
+    // find close entity
+    for (id, pos) in world.query_mut::<&Position>() {
+        if (pos.x - x_world).abs() < 0.5 && (pos.y - y_world).abs() < 0.5 {
+            properties.selected_entity = Option::from(id);
+        }
+    }
+}
+
+fn right_mouse_click(
+    x_screen: i32,
+    y_screen: i32,
+    properties: &mut Properties,
+    entity_events: &mut Vec<EntityEvent>,
+) {
+    // if entity is selected add move event to it with mouse position
+    match properties.selected_entity {
+        None => {
+            return;
+        }
+        Some(entity) => {
+            let x_world = screen_to_world(x_screen, 50);
+            let y_world = screen_to_world(y_screen, 50);
+            entity_events.push(EntityEvent {
+                entity,
+                event_type: StartMove,
+                param: [
+                    (String::from("x"), x_world.to_string()),
+                    (String::from("y"), y_world.to_string()),
+                ]
+                .into(),
+            })
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
+mod input_controller;
 mod window;
 
+use crate::input_controller::InputController;
 use crate::window::Window;
 use crate::EntityEventType::{StartMove, StopMove};
 use hecs::{Entity, World};
@@ -16,87 +18,6 @@ fn world_to_screen(world: f32, zoom_factor: usize) -> i32 {
 
 fn screen_to_world(screen: i32, zoom_factor: usize) -> f32 {
     screen as f32 / zoom_factor as f32
-}
-
-struct InputController {
-    sdl_events: EventPump,
-}
-
-impl InputController {
-    fn new(sdl_context: &Sdl) -> Self {
-        Self {
-            sdl_events: sdl_context.event_pump().unwrap(),
-        }
-    }
-
-    pub fn update(
-        &mut self,
-        properties: &mut Properties,
-        entity_events: &mut Vec<EntityEvent>,
-        world: &mut World,
-    ) {
-        for event in self.sdl_events.poll_iter() {
-            match event {
-                Event::Quit { .. }
-                | Event::KeyDown {
-                    keycode: Some(Keycode::Escape),
-                    ..
-                } => properties.quit = true,
-                Event::MouseButtonUp {
-                    mouse_btn: MouseButton::Left,
-                    x,
-                    y,
-                    ..
-                } => left_mouse_click(x, y, properties, world),
-                Event::MouseButtonUp {
-                    mouse_btn: MouseButton::Right,
-                    x,
-                    y,
-                    ..
-                } => right_mouse_click(x, y, properties, entity_events),
-                _ => {}
-            }
-        }
-    }
-}
-
-fn left_mouse_click(x_screen: i32, y_screen: i32, properties: &mut Properties, world: &mut World) {
-    let x_world = screen_to_world(x_screen, 50);
-    let y_world = screen_to_world(y_screen, 50);
-
-    // find close entity
-    for (id, pos) in world.query_mut::<&Position>() {
-        if (pos.x - x_world).abs() < 0.5 && (pos.y - y_world).abs() < 0.5 {
-            properties.selected_entity = Option::from(id);
-        }
-    }
-}
-
-fn right_mouse_click(
-    x_screen: i32,
-    y_screen: i32,
-    properties: &mut Properties,
-    entity_events: &mut Vec<EntityEvent>,
-) {
-    // if entity is selected add event to it with mouse position
-    match properties.selected_entity {
-        None => {
-            return;
-        }
-        Some(entity) => {
-            let x_world = screen_to_world(x_screen, 50);
-            let y_world = screen_to_world(y_screen, 50);
-            entity_events.push(EntityEvent {
-                entity,
-                event_type: StartMove,
-                param: [
-                    (String::from("x"), x_world.to_string()),
-                    (String::from("y"), y_world.to_string()),
-                ]
-                .into(),
-            })
-        }
-    }
 }
 
 struct Properties {
